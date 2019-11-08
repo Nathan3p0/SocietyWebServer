4c6f6993766b4cdb041a3096ff2585f43e37c0b1
Nathan Szelag <nszelag@live.com>
2019-11-08 10:19:58 -0500

    created jwt-auth file

---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,24 @@
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+    const authToken = req.get('Authorization') || '';
+
+    let bearerToken;
+
+    if (!authToken.toLowerCase().startsWith('bearer ')) {
+        return res.status(400).json(
+            { error: 'Missing bearer token' }
+        );
+    } else {
+        bearerToken = authToken.slice(7, authToken.length);
+    }
+
+    try {
+        const payload = AuthService.verifyJwt(bearerToken);
+
+    } catch (error) {
+        res.status(401).json(
+            { error: 'Unauthorized Request' }
+        )
+    }
+}
