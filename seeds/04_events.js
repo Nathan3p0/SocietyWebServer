29b972798735e58dee63a4d2f0928161cfff149d
Nathan Szelag <nszelag@live.com>
2019-11-13 23:31:07 -0500

    added events and member events seeds

---
+++ seeds/04_events.js
@@ -0,0 +1,11 @@
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('events').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('events').insert([
+        { event_date: '12/01/2019', event_time: '12:00:00', event_name: 'Slime Fest 2019', event_description: 'The biggest slime convention in the world.', event_group: 1 }
+      ]);
+    });
+};
