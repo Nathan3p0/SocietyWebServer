936bd7072946579a9dfffeb5d1697b2e40856422
Nathan Szelag <nszelag@live.com>
2019-11-14 09:26:20 -0500

    added password hashing, database insertion methods, and validation methods

--- src/signup/signup-service.js
+++ src/signup/signup-service.js
@@ -2,7 +2,7 @@ const xss = require('xss');
 const bcrypt = require('bcryptjs');
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
-const signupService = {
+const signupServices = {
     validatePassword(password) {
         if (password.length < 8) {
             return 'Password must be longer than 8 characters.';
@@ -20,6 +20,45 @@ const signupService = {
             return 'Paswword must contain 1 upper case, number and special character'
         }
         return null
+    },
+    hasUserWithUsername(knex, username) {
+        return knex('members')
+            .where({ username })
+            .first()
+            .then(user => !!user);
+    },
+    hasGroupWithGroupName(knex, group_name) {
+        return knex('groups')
+            .where({ group_name })
+            .first()
+            .then(group => !!group);
+    },
+    hashPassword(password) {
+        return bcrypt.hash(password, 10);
+    },
+    insertAdmin(knex, newAdmin) {
+        return knex('members')
+            .insert(newAdmin)
+            .returning('*')
+            .then(([user]) => {
+                return user;
+            });
+    },
+    insertGroup(knex, newGroup) {
+        return knex('groups')
+            .insert(newGroup)
+            .returning('*')
+            .then(([group]) => {
+                return group;
+            });
+    },
+    serializeAdmin(admin) {
+        return {
+            id: admin.id,
+            full_name: xss(admin.full_name),
+            username: xss(admin.username)
+        }
     }
 }
 
+module.exports = signupServices;
