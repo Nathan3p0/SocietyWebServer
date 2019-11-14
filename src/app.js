1027b24d5a4dac30f1dcda30e00c609e0b741c91
Nathan Szelag <nszelag@live.com>
2019-11-14 09:05:24 -0500

    added initial form validation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const signupRouter = require('./signup/signup-router')
 
 const app = express();
 
@@ -12,6 +13,8 @@ const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common');
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/signup', signupRouter)
 
 const errorHandler = (error, req, res, next) => {
     let response;
---
+++ src/signup/signup-router.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const uuidv4 = require('uuid/v4');
+const path = require('path');
+const jsonBodyParser = express.json();
+
+const signupRouter = express.Router();
+
+signupRouter.post('/admin', jsonBodyParser, (req, res, next) => {
+    const { full_name, username, password, email, phone, group_name } = req.body;
+
+    if (!full_name) {
+        return res.status(400).json(
+            { error: 'Please enter a full name.' }
+        );
+    }
+
+    if (!username) {
+        return res.status(400).json(
+            { error: 'Please enter a username.' }
+        );
+    }
+
+    if (!password) {
+        return res.status(400).json(
+            { error: 'Please enter a password.' }
+        );
+    }
+
+    if (!email) {
+        return res.status(400).json(
+            { error: 'Please enter an email.' }
+        )
+    }
+
+    if (!phone) {
+        return res.status(400).json(
+            { error: 'Please enter a phone number.' }
+        )
+    }
+
+    if (!group_name) {
+        return res.status(400).json(
+            { error: 'Please enter a club name.' }
+        )
+    }
+
+    res.send('It passed!')
+})
+
+module.exports = signupRouter;
+
