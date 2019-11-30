7a58b5e7f2180635c63916a60d7230b7005b5892
Nathan Szelag <nszelag@live.com>
2019-11-29 19:22:25 -0500

    updated members router to fetch member info

--- src/routes/teamMembers/team-members-router.js
+++ src/routes/teamMembers/team-members-router.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const jsonBodyParser = express.json();
+const xss = require('xss');
 const requireAuth = require('../../middleware/jwt-auth');
 const teamMemberService = require('./team-member-service');
 const teamMembersRouter = express.Router();
+
+teamMembersRouter.get('/member', requireAuth, (req, res, next) => {
+    const db = req.app.get('db');
+    const user = req.user;
+
+    teamMemberService.findGroupByAdminId(db, user.id)
+        .then(group => {
+            if (!group) {
+                return res.status(404).json({
+                    error: 'There was no group found for this account.'
+                });
+            }
+            return res.status(200).json({
+                group_name: xss(group.group_name),
+                name: xss(user.full_name)
+            })
+        })
+})
 
 teamMembersRouter.get('/', requireAuth, (req, res, next) => {
     const db = req.app.get('db');
